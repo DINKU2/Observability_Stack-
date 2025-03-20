606702bf10cd721ec51060c771bbdb8da8b04666
Dinuk <dinukkarunartne747@gmail.com>
2025-03-20 15:44:50 -0500

    sending trace data to collector

--- fastapi-telemetry/api1/app/main.py
+++ fastapi-telemetry/api1/app/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
 import httpx
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
 
+# Initialize tracing
+resource = Resource.create({"service.name": "api1"})
+tracer_provider = TracerProvider(resource=resource)
+otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True)
+tracer_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+trace.set_tracer_provider(tracer_provider)
+
+# Create FastAPI app
 app = FastAPI()
+
+# Instrument FastAPI
+FastAPIInstrumentor.instrument_app(app)
+
+# Instrument HTTPX
+HTTPXClientInstrumentor().instrument()
 
 @app.get("/")
 async def read_root():
--- fastapi-telemetry/api2/app/main.py
+++ fastapi-telemetry/api2/app/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
 
+# Initialize tracing
+resource = Resource.create({"service.name": "api2"})
+tracer_provider = TracerProvider(resource=resource)
+otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True)
+tracer_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+trace.set_tracer_provider(tracer_provider)
+
+# Create FastAPI app
 app = FastAPI()
+
+# Instrument FastAPI
+FastAPIInstrumentor.instrument_app(app)
 
 @app.get("/")
 async def read_root():
